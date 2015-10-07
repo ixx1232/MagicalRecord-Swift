9485169833f57b4a07c3dab5bbd7d794e3063fa4
Jacky Wu <jayst088@gmail.com>
2015-10-06 21:23:56 -0700

    first init

---
+++ MagicalRecord-Swift/MagicalRecord-Swift.h
@@ -0,0 +1,19 @@
+//
+//  MagicalRecord-Swift.h
+//  MagicalRecord-Swift
+//
+//  Created by Jacky Wu on 10/6/15.
+//  Copyright © 2015 Jacky Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MagicalRecord-Swift.
+FOUNDATION_EXPORT double MagicalRecord_SwiftVersionNumber;
+
+//! Project version string for MagicalRecord-Swift.
+FOUNDATION_EXPORT const unsigned char MagicalRecord_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagicalRecord_Swift/PublicHeader.h>
+
+
