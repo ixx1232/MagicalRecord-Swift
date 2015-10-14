8698885537a21f6d83fd5a6a3ebb3087403d292c
Jacky Wu <jayst088@gmail.com>
2015-10-14 01:34:20 -0700

    temporary setup logging level vars
    Still need to setup enum

--- MagicalRecord-Swift/MagicalRecord+Options.swift
+++ MagicalRecord-Swift/MagicalRecord+Options.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+import CoreData
+
+public enum MagicalRecordLoggingMask: UInt {
+  case MagicalRecordLoggingMaskError = 0b00001
+  /** Log warnings, and all errors */
+  case MagicalRecordLoggingMaskWarn = 0b00010
+  /** Log informative messagess, warnings and all errors */
+  case MagicalRecordLoggingMaskInfo = 0b00100
+  /** Log debugging messages, informative messages, warnings and all errors */
+  case MagicalRecordLoggingMaskDebug = 0b01000
+  /** Log verbose diagnostic information, debugging messages, informative messages, messages, warnings and all errors */
+  case MagicalRecordLoggingMaskVerbose = 0b10000
+}
+
+public enum MagicalRecordLoggingLevel: UInt {
+  /** Don't log anything */
+//  case MagicalRecordLoggingLevelOff = 0
+  /** Log all errors and fatal messages */
+//  case MagicalRecordLoggingLevelError =
+  /** Log warnings, errors and fatal messages */
+//  case MagicalRecordLoggingLevelWarn = (MagicalRecordLoggingLevelError | MagicalRecordLoggingMaskWarn)
+  /** Log informative, warning and error messages */
+//  case MagicalRecordLoggingLevelInfo = (MagicalRecordLoggingLevelWarn | MagicalRecordLoggingMaskInfo)
+  /** Log all debugging, informative, warning and error messages */
+//  case MagicalRecordLoggingLevelDebug = (MagicalRecordLoggingLevelInfo | MagicalRecordLoggingMaskDebug)
+  /** Log verbose diagnostic, debugging, informative, warning and error messages */
+//  case MagicalRecordLoggingLevelVerbose = (MagicalRecordLoggingLevelDebug | MagicalRecordLoggingMaskVerbose)
+  /** Log everything */
+  case MagicalRecordLoggingLevelAll = 1
+}
+
+//MARK: MagicalRecord+Options
+public extension MagicalRecord {
+  static var shouldAutoCreateManagedObjectModel: Bool = false
+  static var shouldAutoCreateDefaultPersistentStoreCoordinator: Bool = false
+  static var shouldDeleteStoreOnModelMismatch: Bool = false
+  
+  #if DEBUG
+  static var loggingLevel: MagicalRecordLoggingLevel = MagicalRecordLoggingLevelDebug
+  #else
+  static var loggingLevel: MagicalRecordLoggingLevel = MagicalRecordLoggingMaskError
+  #endif
+}
+
+
+
+
+
+
+ 
