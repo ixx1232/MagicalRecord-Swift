5f49e26924a47f3df88ddad8a65fd61a39c11b25
Jacky Wu <jayst088@gmail.com>
2015-10-14 00:45:17 -0700

    draft implementation of magical record internal and setup

--- MagicalRecord-Swift/MagicalRecord+Setup.swift
+++ MagicalRecord-Swift/MagicalRecord+Setup.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+import CoreData
+
+//MARK: MagicalRecord+Setup
+public extension MagicalRecord {
+  
+  public class func setupCoreDataStack() {
+    setupCoreDataStackWithStoreNamed(defaultStoreName())
+  }
+  
+  public class func setupAutoMigratingCoreDataStack() {
+    setupCoreDataStackWithAutoMigratingSqliteStoreNamed(defaultStoreName())
+  }
+
+  public class func setupCoreDataStackWithStoreNamed(storeName: String) {
+    if (NSPersistentStoreCoordinator.MR_defaultStoreCoordinator() == nil) {
+        let coordinator = NSPersistentStoreCoordinator.MR_coordinatorWithSqliteStoreNamed(storeName)
+        NSPersistentStoreCoordinator.MR_setDefaultStoreCoordinator(coordinator)
+        NSManagedObjectContext.MR_initializeDefaultContextWithCoordinator(coordinator)
+    }
+  }
+
+  public class func setupCoreDataStackWithAutoMigratingSqliteStoreNamed(storeName: String) {
+    if (NSPersistentStoreCoordinator.MR_defaultCoordinator() == nil) {
+      let coordinator = NSPersistentStoreCoordinator.MR_coordinatorWithAutoMigratingSqliteStoreNamed(storeName)
+      NSPersistentStoreCoordinator.MR_setDefaultStoreCoordinator(coordinator)
+      NSManagedObjectContext.MR_initializeDefaultContextWithCoordinator(coordinator)
+    }
+  }
+  
+  public class func setupCoreDataStackWithStoreAtURL(storeURL: NSURL) {
+    if (NSPersistentStoreCoordinator.MR_defaultCoordinator() == nil) {
+      let coordinator = NSPersistentStoreCoordinator.MR_coordinatorWithSqliteStoreAtURL(storeURL)
+      NSPersistentStoreCoordinator.MR_setDefaultStoreCoordinator(coordinator)
+      NSManagedObjectContext.MR_initializeDefaultContextWithCoordinator(coordinator)
+    }
+  }
+  
+  public class func setupCoreDataStackWithAutoMigratingSqliteStoreAtURL(storeURL: NSURL) {
+    if (NSPersistentStoreCoordinator.MR_defaultCoordinator() == nil) {
+      let coordinator = NSPersistentStoreCoordinator.MR_coordinatorWithAutoMigratingSqliteStoreAtURL(storeURL)
+      NSPersistentStoreCoordinator.MR_setDefaultStoreCoordinator(coordinator)
+      NSManagedObjectContext.MR_initializeDefaultContextWithCoordinator(coordinator)
+    }
+  }
+
+  public class func setupCoreDataStackWithInMemoryStore() {
+    if (NSPersistentStoreCoordinator.MR_defaultCoordinator() == nil) {
+      let coordinator = NSPersistentStoreCoordinator.MR_coordinatorWithInMemoryStore()
+      NSPersistentStoreCoordinator.MR_setDefaultStoreCoordinator(coordinator)
+      NSManagedObjectContext.MR_initializeDefaultContextWithCoordinator(coordinator)
+    }
+  }
+}
--- MagicalRecord-Swift/MagicalRecordInternal.swift
+++ MagicalRecord-Swift/MagicalRecordInternal.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreData
 
 //#if NS_BLOCKS_AVAILABLE
 //  extern NSString * const kMagicalRecordCleanedUpNotification;
@@ -22,34 +23,88 @@ public enum MagicalRecordVersionTag: UInt {
 
 public class MagicalRecord {
   
+  
+  /*
+
++ (void) initialize;
+{
+if (self == [MagicalRecord class])
+{
+[self setShouldAutoCreateManagedObjectModel:YES];
+[self setShouldAutoCreateDefaultPersistentStoreCoordinator:NO];
+#ifdef DEBUG
+[self setShouldDeleteStoreOnModelMismatch:YES];
+#else
+[self setShouldDeleteStoreOnModelMismatch:NO];
+#endif
+}
+}
+  */
+
   public class func version() -> MagicalRecordVersionTag {
     return MagicalRecordVersionTag.MagicalRecordVersionTag2_2
   }
-
-  public class func cleanup() {
+  
+  public class func cleanUp() {
+    cleanErrorHandling()
+    cleanUpStack()
+    let notificationCenter = NSNotificationCenter.defaultCenter()
+    notificationCenter.postNotificationName(kMagicalRecordCleanedUpNotification, object: nil, userInfo: nil)
+  }
+  
+  private class func cleanUpStack() {
+    NSManagedObjectContext.MR_cleanUP()
+    NSManagedObjectModel.MR_setDefaultManagedObjectModel(nil)
+    NSPersistentStoreCoordinator.MR_setDefaultStoreCoordinator(nil)
+    NSPersistentStore.MR_setDefaultPersistentStore(nil)
+  }
+  
+  /*
+  private class func cleanErrorHandling() {
     
   }
   
-  public class func currentStack() -> String {
+  private class func MR_cleanUP() {
+    
+  }
+  */
 
+  public class func currentStack() -> String {
+    var status = "Current Default Core Data Stack: ----\n"
+    status.appendContentsOf("Model: \(NSManagedObjectModel.MR_defaultManagedObjectModel().entityVersionHashesByName()))")
+    status.appendContentsOf("Coordinator: \(NSPersistentStoreCoordinator.MR_defaultStoreCoordinator())")
+    status.appendContentsOf("Store: \(NSPersistentStore.MR_defaultPersistentStore())")
+    status.appendContentsOf("Default Context: \(NSManagedObjectContext.MR_defaultContext()?.MR_description())")
+    status.appendContentsOf("Context Chain: \(NSManagedObjectContext.MR_defaultContext()?.MR_parentChain())\n")
+    return status
   }
   
   public class func setDefaultModelNamed(modelName: String) {
-
+    if let model = NSManagedObjectModel.MR_managedObjectModelNamed(modelName) {
+       NSManagedObjectModel.MR_setDefaultManagedObjectModel(model)
+    }
   }
 
   public class func setDefaultModelFromClass(modelClass: AnyClass) {
-    
+    let bundle = NSBundle(forClass: modelClass)
+    if let model = NSManagedObjectModel.mergedModelFromBundles([bundle]) {
+      NSManagedObjectModel.MR_setDefaultManagedObjectModel(model)
+    }
   }
-
-  public class func defaultStoreName() -> String! {
+  
+  public class func defaultStoreName() -> String {
     var defaultName: String? = NSBundle.mainBundle().objectForInfoDictionaryKey(kCFBundleNameKey as String) as? String
     if (defaultName == nil) {
       defaultName = kMagicalRecordDefaultStoreFileName
     }
-    if (defaultName!.hasSuffix("sqlite")) {
-      defaultName = (defaultName! as NSString).stringByAppendingPathExtension("sqlite")
+    if var defaultName = defaultName where defaultName.hasSuffix("sqlite") {
+      defaultName = (defaultName as NSString).stringByAppendingPathComponent("sqlite")
     }
     return defaultName!
   }
 }
+
+
+
+
+
